c3c64f3f06dea08b6b729e2c3403fb991a1ffb52
Roman <romanf2288@gmail.com>
2026-02-18 23:41:38 +0500

    Обновление маршрутов для системы авторизации

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,3 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::post('/register', [AuthController::class, 'register']);
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', fn () => view('index'))->name('index');
+Route::get('/register', fn () => view('register'))->name('register');
+Route::get('/login', fn () => view('login'))->name('login');
+
+Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::get('/profile', fn () => view('profile'))->name('profile');
+});
