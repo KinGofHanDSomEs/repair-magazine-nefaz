ae790d6fac94ac2827e974b8616f4d9cedb7a320
Roman <romanf2288@gmail.com>
2026-03-03 17:49:15 +0500

    Изменение путей сайтов

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,41 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\OrderController as OrderControllerAlias;
+use App\Http\Controllers\UserController;
+use App\Models\Technic;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('index'))->name('index');
-Route::get('/register', fn () => view('register'))->name('register');
-Route::get('/login', fn () => view('login'))->name('login');
+Route::get('/', function () {
+    $technics = Technic::all();
 
-Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
-Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+    return view('index', compact('technics'));
+})->name('index');
 
+Route::get('/register', [AuthController::class, 'showRegister'])->name('auth.register');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('auth.login');
+
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
-    Route::get('/profile', fn () => view('profile'))->name('profile');
-    Route::get('/orders', [OrderControllerAlias::class, 'orders'])->name('orders');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/profile', [AuthController::class, 'showProfile'])->name('auth.profile');
+    Route::patch('/profile/{profile}/change', [AuthController::class, 'changeProfile'])->name('auth.profile.change');
+    Route::patch('/profile/{profile}/changePassword', [AuthController::class, 'changeProfilePassword'])->name('auth.profile.changePassword');
+
+    Route::resource('/orders', OrderController::class);
+    Route::post('/orders/{order}/pay', [OrderController::class, 'pay'])->name('orders.pay'); // TODO: payment system!?)
+
+    Route::middleware('admin')->group(function () {
+        Route::patch('/orders/{order}/start', [OrderController::class, 'start'])->name('orders.start');
+        Route::patch('/orders/{order}/complete', [OrderController::class, 'complete'])->name('orders.complete');
+        Route::patch('/orders/{order}/rate', [OrderController::class, 'rate'])->name('orders.rate');
+        Route::patch('/orders/{order}/revoke', [OrderController::class, 'revoke'])->name('orders.revoke');
+
+        Route::resource('/users', UserController::class);
+        Route::patch('/users/{user}/confirm', [UserController::class, 'confirm'])->name('users.confirm');
+    });
 });
