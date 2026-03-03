69de8834993253fbafd5d71404b54126e3bf8800
Roman <romanf2288@gmail.com>
2026-03-03 17:48:12 +0500

    Обновление моделей

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,32 +2,60 @@
 
 namespace App\Models;
 
+use App\Traits\FormattedDates;
+use Database\Factories\OrderFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\HigherOrderCollectionProxy;
 
+
+/**
+ *@mixin Builder
+ */
 class Order extends Model
 {
-    /** @use HasFactory<\Database\Factories\OrderFactory> */
-    use HasFactory, Notifiable, SoftDeletes;
+    /** @use HasFactory<OrderFactory> */
+    use HasFactory, Notifiable, SoftDeletes, FormattedDates;
+
+    /**
+     * @var HigherOrderCollectionProxy|mixed
+     */
 
     protected $fillable = [
         'user_id',
-        'technic_id',
+        'type_id',
+        'count',
         'status',
         'price',
         'payment_status',
         'failed_message',
+        'created_at',
+        'completed_at',
+        'updated_at',
         'completed_at'
+    ];
+
+    protected $hidden = [
+        'deleted_at',
     ];
 
     public function user(): BelongsTo {
         return $this->belongsTo(User::class);
     }
 
-    public function technic(): BelongsTo {
-        return $this->belongsTo(Technic::class);
+    public function type(): BelongsTo {
+        return $this->belongsTo(Type::class);
+    }
+    public function casts(): array
+    {
+        return [
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+            'completed_at' => 'datetime',
+        ];
     }
 }
